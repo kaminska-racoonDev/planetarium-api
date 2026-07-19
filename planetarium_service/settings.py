3d77e6fddebfa4dffc3e2128f7c69bbe00c20034
JulianskyMe <juliakamynskaya@gmail.com>
2026-07-19 23:08:34 +0300

    Disable DEBUG in settings

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -24,11 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-w&x*iakssh8=!v)su&r@"
 "lwdcyh@j4r7a561ao&^tkfw5$1--l1"
 
-# SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = True
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 # Application definition
 
